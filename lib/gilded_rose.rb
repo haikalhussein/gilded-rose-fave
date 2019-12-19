a7583114968145c1d5069c4cdea852f4a6b36ed0
haikalhussein <mhaikal.mhussein@gmail.com>
2019-12-19 10:04:48 +0800

    Added constance name for items.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,18 +3,22 @@ class GildedRose
     @items = items
   end
 
+  AGED_BRIE = "Aged Brie"
+  BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+
   def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != AGED_BRIE && item.name != BACKSTAGE_PASS
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          if item.name != SULFURAS
             item.quality = item.quality - 1
           end
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.name == BACKSTAGE_PASS
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -28,14 +32,14 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      if item.name != SULFURAS
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != AGED_BRIE
+          if item.name != BACKSTAGE_PASS
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              if item.name != SULFURAS
                 item.quality = item.quality - 1
               end
             end
