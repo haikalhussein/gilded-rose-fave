7148a69b395fe04e7688eed10720d9dd71b61e97
haikalhussein <mhaikal.mhussein@gmail.com>
2019-12-19 10:19:41 +0800

    Added method called decrease_quality.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -44,19 +44,20 @@ class GildedRose
           end
         elsif item.name == BACKSTAGE_PASS
           item.quality = item.quality - item.quality
-
         elsif item.name == SULFURAS
-          # DO NOTHING
+          #DO NOTHING
         else
-          
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-          
+          decrease_quality(item)
         end
-
       end
 
     end
   end
+
+  def decrease_quality(item)
+    if item.quality > 0
+      item.quality = item.quality - 1
+    end
+  end
+
 end
