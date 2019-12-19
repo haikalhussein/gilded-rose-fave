258084a8f7b1ff0c924ad5ef7bac098f20e761e8
haikalhussein <mhaikal.mhussein@gmail.com>
2019-12-19 11:38:40 +0800

    Introducing new class ItemUpdater and re-positioning expired and update_item_quality method outside of update_quality block.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,6 +7,20 @@ class GildedRose
   BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
   SULFURAS = "Sulfuras, Hand of Ragnaros"
 
+  class ItemUpdater
+    attr_reader :item, :quality_delta
+
+    def initialize(item, quality_delta)
+      @item = item
+      @quality_delta = quality_delta
+    end
+
+    def update
+      update_item_quality(item, quality_delta)
+      update_item_quality(item, quality_delta) if expired?(item)
+    end
+  end
+
   def update_quality
     @items.each do |item|
 
@@ -15,7 +29,6 @@ class GildedRose
       end
 
       case item.name
-
       when BACKSTAGE_PASS
         update_item_quality(item, 1)
         if item.name == BACKSTAGE_PASS
@@ -28,26 +41,22 @@ class GildedRose
         end
         update_item_quality(item, -item.quality) if expired?(item)
       when AGED_BRIE
-        update_item_quality(item, 1)
-        update_item_quality(item, 1) if expired?(item)
+        ItemUpdater.new(item, 1).update
       when SULFURAS
         #DO NOTHING
       else
-        update_item_quality(item, -1)
-        update_item_quality(item, -1) if expired?(item)
+        ItemUpdater.new(item, -1).update
       end
-
     end
   end
+end
 
-  def expired?(item)
+def expired?(item)
     item.sell_in < 0
   end
 
-  def update_item_quality(item, quality_delta)
+def update_item_quality(item, quality_delta)
     if item.quality < 50 && item.quality > 0
       item.quality += quality_delta
     end
   end
-
-end
